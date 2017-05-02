74977936e21905785b1a881ce5872f3c591fe2fd
lisa louison <lisa.louison@gmail.com>
2017-05-02 16:25:31 -0400

    updated config file

--- db/config.js
+++ db/config.js
@@ -1,5 +1,25 @@
+// setting up a variable that logs every query
+const options = {
+  connect: (client, dc, isFresh) => {
+    // do this every time the database connects
+    console.log('Connected to database:', client.connectionParameters.database)
+  },
+  query: (e) => {
+    // do this every time a query is made to the database
+    console.log('making query ========> ' + e.query);
+  },
+  receive: (data, result, e) => {
+    // do this every time the app receives data from the database
+    console.log('completed query =======> ' + e.query);
+  },
+  disconnect: (client, dc) => {
+    // do this every time the database disconnects (usually after every query)
+    console.log('Disconnecting from database:', client.connectionParameters.database);
+  }
+};
+
 // importing pg-promise
-const pgp = require('pg-promise')();
+const pgp = require('pg-promise')(options);
 
 // setting a variable for pg-promise
 let db;
