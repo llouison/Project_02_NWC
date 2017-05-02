5ea078c3a31964d7b3a9f0ae05907cfe321ba145
lisa louison <lisa.louison@gmail.com>
2017-05-02 15:08:01 -0400

    created recipe controller

---
+++ controllers/recipesController.js
@@ -0,0 +1,89 @@
+// importing the recipe model
+const Recipe = require('../models/recipe');
+
+// creating a controller object
+const controller = {};
+
+// defining the view to render once the findall promise is complete
+controller.index = (req, res) => {
+    Recipe.findAll()
+    .then(recipes => {
+        res.render('recipes/recipes-index', {
+            documentTitle: 'Now We\'re Cookin\'',
+            recipesData: recipes,
+        });
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    });
+};
+
+// defining the view to render once the findbyid promise is complete
+controller.show = (req, res) => {
+    Recipe.findById(req.params.id)
+    .then(recipe => {
+        res.render('recipes/recipes-single', {
+            documentTitle: `Now We're Cookin' - ${id}`,
+            recipe: recipe,
+        });
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    });
+};
+
+// defining the view to render once the create new recipe promise is complete
+controller.create = (req, res) => {
+    Recipe.create({
+        title: req.body.title,
+        author: req.body.author,
+        description: req.body.description,
+        category_type: req.body.category_type,
+    })
+    .then(recipe => {
+        res.status(400).json(err);
+    });
+};
+
+// defining the view to render once the edit recipe promise is complete
+controller.edit = (req, res) => {
+    Recipe.findById(req.params.id)
+    .then(recipe => {
+        console.log(recipe);
+        res.redner('recipes/recipes-edit', {
+            documentTitle: `Now We're Cookin' - edit ${id}`,
+            recipe: recipe,
+            id: req.params.id,
+        });
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    });
+};
+
+// defining the view to render once the update recipe promise is complete
+controller.update = (req, res) => {
+    Recipe.update({
+        title: req.body.title,
+        author: req.body.author,
+        description: req.body.description,
+        category_type: req.body.category_type,
+    }, req.params.id)
+    .then(recipe => {
+        res.status(400).json(err);
+    });
+};
+
+// defining the view to render once the delete promise is complete
+controller.destroy = (req, res) => {
+    Recipe.destroy(req.params.id)
+    .then(() => {
+        res.redirect('/recipes');
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    });
+};
+
+//exporting the controller
+module.exports = controller;
