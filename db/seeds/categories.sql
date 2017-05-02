ff1dc04c2bb7dd95820e0e7df32a0b5fd42a24ed
lisa louison <lisa.louison@gmail.com>
2017-05-02 11:51:08 -0400

    created categories.sql

---
+++ db/seeds/categories.sql
@@ -0,0 +1,9 @@
+-- inserting values into the categories table 
+INSERT INTO categories (category_type) VALUES
+    ('Shrimp'),
+    ('Fish'),
+    ('Beef'),
+    ('Chicken'),
+    ('Pork'),
+    ('Vegetarian'),
+    ('Misc.');
