a616bf2f72e391298fead51d87881bc5d0be370f
lisa louison <lisa.louison@gmail.com>
2017-05-02 11:47:15 -0400

    created migration file

---
+++ db/migrations/migration_05022017.sql
@@ -0,0 +1,15 @@
+-- creating the categories and recipes tables in the recipe development database
+\connect recipe_development
+
+CREATE TABLE IF NOT EXISTS categories (
+    id BIGSERIAL PRIMARY KEY,
+    category_type VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS recipes (
+    id BIGSERIAL PRIMARY KEY,
+    title VARCHAR(255),
+    author VARCHAR(255),
+    description VARCHAR(1028),
+    category_id INTEGER REFERENCES categories(id)
+);
