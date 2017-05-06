53d4b84dc6022e825f927c0f49e7518cdabbacb4
lisa louison <lisa.louison@gmail.com>
2017-05-06 12:04:17 -0400

    updated config file

--- db/config.js
+++ db/config.js
@@ -25,18 +25,18 @@ const pgp = require('pg-promise')(options);
 let db;
 
 // creating if statements to determine the instance of pgp to use
-const pgp = require('pg-promise')();
-
-let db;
-
 if (process.env.NODE_ENV === 'development' || !process.env.NODE_ENV) {
-  db = pgp({
-    database: 'adaquote_development',
-    port: 5432,
-    host: 'localhost',
-  });
+    db = pgp({
+        database: 'recipe_development',
+        port: 5432,
+        host: 'localhost'
+    });
 } else if (process.env.NODE_ENV === 'production') {
-  db = pgp(process.env.DATABASE_URL);
+    db = pgp({
+        database: 'recipe_production',
+        port: 5432,
+        host: 'localhost'
+    });
 }
 
 //exporting pg-promise
