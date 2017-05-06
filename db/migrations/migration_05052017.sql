fa7c86ce4effee699c508ffb0211e23d3e5d977b
lisa louison <lisa.louison@gmail.com>
2017-05-06 11:58:38 -0400

    created default value for ingredients

--- db/migrations/migration_05052017.sql
+++ db/migrations/migration_05052017.sql
@@ -1,7 +1,7 @@
 -- connecting to the recipe development database
 \connect recipe_development
 
--- adding the ingredients column to the recipe development database
+-- altering the datatype of the ingredients column in the recipes table
 ALTER TABLE recipes 
 ALTER COLUMN ingredients 
-SET DATA TYPE ingredients text[] not null default '{}';
+SET DEFAULT '{}';
