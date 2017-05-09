9300505ca753814ea206e95f810ea0ca9a7b256a
lisa louison <lisa.louison@gmail.com>
2017-05-08 21:46:43 -0400

    added comments to user route

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,9 @@
+// importing express
 const express = require('express');
-
+// setting a variable for the express router
 const userRoutes = express.Router();
 
+// setting a route for the user profile view
 userRoutes.get('/', (req, res) => {
   res.json({ user: 'user profile page placeholder', userInfo: req.user });
 });
