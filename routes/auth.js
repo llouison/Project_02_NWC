707bda5ec30bd8881b462e7f9df615a1dc7ad269
lisa louison <lisa.louison@gmail.com>
2017-05-08 21:49:56 -0400

    added comments to auth route

--- routes/auth.js
+++ routes/auth.js
@@ -1,21 +1,28 @@
+// importing express
 const express = require('express');
+// importing the users controller
 const controller = require('../controllers/usersController');
-
+// setting a variable for the express router
 const router = express.Router();
-
+// importing the auth helper
 const authHelpers = require('../services/auth/authHelpers');
 const passport = require('../services/auth/local');
 
+// setting a route for the user login view
 router.get('/login', (req, res) => {
   res.render('auth/log-in', {
     message: 'User Login',
   });
 });
+
+// setting a route for the user registration view
 router.get('/register', (req, res) => {
   res.render('auth/register', {
     message: 'User Registration',
   });
 });
+
+// setting a route for the create user function
 router.post('/register', controller.create);
 router.post(
   '/login',
@@ -25,9 +32,12 @@ router.post(
     failureFlash: false,
   })
 );
+
+// setting a route for the user logout function
 router.get('/logout', (req, res) => {
   req.logout();
   res.redirect('/');
 });
 
+// exporting the router
 module.exports = router;
