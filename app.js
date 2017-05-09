40cb30ea6fa4dcd2fed4251f13001c0f4e1436bc
lisa louison <lisa.louison@gmail.com>
2017-05-08 21:16:45 -0400

    imported user authentication modules/routes

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ const path = require('path');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const passport = require('passport');
+
+// importing dotenv config function
+require('dotenv').config();
 
 // creating a variable for the express function
 const app = express();
@@ -12,6 +18,8 @@ const app = express();
 const recipeHelpers = require('./services/recipes/recipeHelpers');
 // importing routes
 const recipesRoutes = require('./routes/recipesRoutes');
+const authRoutes = require('./routes/auth');
+const userRoutes = require('./routes/users')
 
 // setting up port for express to listen to for activity
 const PORT = process.env.PORT || 3000;
@@ -26,13 +34,24 @@ app.set('view engine', 'ejs');
 
 // directing express to static files
 app.use('/static', express.static(path.join(__dirname, 'public')));
-// setting uo the morgan logger function to run on dev script
+// setting up the morgan logger function to run on dev script
 app.use(logger('dev'));
-// setting uo the body parser function to run on json info
+// setting up the cookie parser module
+app.use(cookieParser());
+// setting up the body parser function to run on json info
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 // setting up the method override module to run on http method key word
 app.use(methodOverride('_method'));
+// setting up the express session module
+app.use(session({
+  secret: process.env.SECRET_KEY,
+  resave: false,
+  saveUninitialized: true
+}));
+// setting up the passport module
+app.use(passport.initialize());
+app.use(passport.session());
 
 // setting up the route to the index page
 app.get('/',(req, res) => {
@@ -55,6 +74,10 @@ app.get('/search', recipeHelpers.getRecipe, (req, res) => {
 
 // directing the app to use the recipesRoutes for all recipes urls
 app.use('/recipes', recipesRoutes);
+// directing the app to use the authRoutes for user authentication
+app.use('/auth', authRoutes);
+// directing the app to use the userRoutes for users
+app.use('/user', userRoutes);
 
 // handling 404 errors
 app.get('*', (req, res) => {
